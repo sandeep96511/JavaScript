4b5cc215bb4115501648ab70ff8979b25ac405f1
SANDEEP <sandeepsingh96511@gmail.com>
2024-06-06 00:58:02 +0530

    link fixed

---
+++ Emoji Finder/script.js
@@ -0,0 +1,43 @@
+let input = document.querySelector('input');
+let container = document.querySelector('.container');
+
+//todo ---------- Load all the emojis on UI ----------
+window.addEventListener('load', function(e){
+    emojiList.forEach((element) => {
+        let div = document.createElement('div');
+        div.innerHTML = `
+        <p class= "emoji"> ${element.emoji} </p>
+        <p class = "name"> ${element.aliases.join(", ")} </p>
+        <p class = "name"> ${element.description} </p>
+
+        `;
+        div.classList.add('emojis');
+        container.appendChild(div);
+    });
+});
+
+//todo ---------- Search & filter the emojis ----------
+input.addEventListener('keyup', function(e){
+    let value = e.target.value.toLowerCase();  
+
+    let filterArr = emojiList.filter((element) => {
+        return element.aliases[0].toLowerCase().includes(value);
+    });
+
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    for(let i = 0; i < filterArr.length; i++){
+        let emoji = filterArr[i].emoji;
+        let aliases = filterArr[i].aliases[0];
+
+        let div = document.createElement('div');
+        div.innerHTML = `
+        <p class= "emoji"> ${emoji} </p>
+        <p class = "name"> ${aliases} </p>
+        `;
+        div.classList.add('emojis');
+        container.appendChild(div);
+    }
+});
