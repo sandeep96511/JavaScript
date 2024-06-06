0168ef512081c643ab14492260e78b150070b37d
SANDEEP <sandeepsingh96511@gmail.com>
2024-06-06 20:19:33 +0530

    minor changes

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -7,6 +7,11 @@ let hh = document.getElementById("hh");
 let mm = document.getElementById("mm");
 let ss = document.getElementById("ss");
 
+let hr_dot = document.querySelector(".hr_dot");
+let min_dot = document.querySelector(".min_dot");
+let sec_dot = document.querySelector(".sec_dot");
+
+
 
 function updateClock() {
   let h = new Date().getHours();
@@ -31,6 +36,11 @@ function updateClock() {
   hh.style.strokeDashoffset = 440 - (440 * h) / 12;
   mm.style.strokeDashoffset = 440 - (440 * m) / 60;
   ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+  hr_dot.style.transform = `rotate(${h*30}deg)`;
+  min_dot.style.transform = `rotate(${m*6}deg)`;
+  sec_dot.style.transform = `rotate(${s*6}deg)`;
+
 
   setTimeout(() => {
     updateClock();
